878afd2588180baca92f21cde0c92674c0019d98
kade042 <dereje88@gmail.com>
2016-10-05 17:30:48 -0600

    adding test user, item, and category to seed

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sprockets-rails'
 gem 'compass-rails'
 gem 'jquery-turbolinks'
 gem "bootstrap-sass"
+gem "faker"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.delete_all
+Item.delete_all
+
+10.times do
+  Item.create(
+    title: Faker::Commerce.product_name,
+    inventory: Faker::Number.number(2),
+    price: Faker::Number.number(4),
+    #image: Faker::Avatar.image,
+    description: Faker::StarWars.quote
+  )
+
+  Category.create(title: Faker::Commerce.department)
+
+end
+
+
+counter = 1
+Item.all.each do |item|
+  item.category_id = counter
+  item.save
+  counter += 1
+end
+
+
+10.times do |u|
+  User.create(
+  name: "user#{u+1}",
+  email: "user#{u+1}@email.com",
+  password: "password"
+  )
+end
