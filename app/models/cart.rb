d443234d04b930a196e5121a0041d9326ebb73c0
kade042 <dereje88@gmail.com>
2016-10-08 20:01:39 -0600

    adding cart show page

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,10 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
   has_many :items, through: :line_items
+
+  def total
+    line_items.collect { |li|
+      li.valid? ? (li.quantity * li.item.price) : 0
+    }.sum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'carts/show'
-
+  
+  root to: 'store#index', as: 'store'
   resources :line_items #, only: [:update]
   resources :items
   resources :categories, only: :show
-  root to: 'store#index', as: 'store'
+  resources :carts, only: :show
+  
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
