0961fdbb2969b06f0595790c4e59b9315432a767
kade042 <dereje88@gmail.com>
2016-10-10 23:36:05 -0600

    handling error like route, not found ..

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,35 @@ class ApplicationController < ActionController::Base
    include Pundit
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_cart
-
-
+  
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from Exception, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
   rescue_from Pundit::NotAuthorizedError, with: :not_found
+  
+  def raise_not_found
+    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+  end
 
   protected
+
+  
+
+
+  
 
   def not_found
     respond_to do |format|
       format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
+      format.xml { head :not_found }
+      format.any { head :not_found }
+    end
+  end
+
+  def error
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/500", :layout => false, :status => :error }
       format.xml { head :not_found }
       format.any { head :not_found }
     end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :items
   resources :categories, only: :show
   resources :carts, only: :show
-  
+  get '*unmatched_route', to: 'application#raise_not_found'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
