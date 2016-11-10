4ce87217d141e0d78f1daf42d51a64f15b5984bc
kade042 <dereje88@gmail.com>
2016-11-10 13:17:32 -0700

    modifing show page to show customer reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011220914) do
+ActiveRecord::Schema.define(version: 20161110195737) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
@@ -66,6 +66,17 @@ ActiveRecord::Schema.define(version: 20161011220914) do
     t.datetime "updated_at",      null: false
     t.index ["order_status_id"], name: "index_orders_on_order_status_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "shipping_addresses", force: :cascade do |t|
